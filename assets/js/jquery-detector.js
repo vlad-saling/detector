77c1b7e0577a2707b16d31664a7034f08e99f17b
vlad saling <vladislavsaling@gmail.com>
2013-09-10 15:05:27 +0200

    refining

--- assets/js/jquery-detector.js
+++ assets/js/jquery-detector.js
@@ -1,24 +1,22 @@
-$(window).resize(function() {
-  // This will execute whenever the window is resized
-
-  $(window).height(); // New height
-  $(window).width(); // New width
-
-  
-});
-
 $(document).ready(function(){
 
+//screen dimensions
 $("#width").html(window.screen.width);
 $("#height").html(window.screen.height);
+
+
+//viewport dimensions
 $("#wwidth").html(window.innerWidth);
 $("#wheight").html(window.innerHeight);
+
+
+//dpr/dpi calculation
 $("#dpr").html(window.devicePixelRatio);
 $("#dpi--value").html(window.devicePixelRatio*96);
 
 
+//em value check
 $("#em--value").append("<span style=\"display:inline-block; padding:0; line-height:1; position:absolute; visibility:hidden; font-size:1em\">M</span>");
-
 var emheight = $("#em--value span").height();
 $("#em--value").html(emheight);
 
